257a47aa2b33bac41222e77a379a9abb0b55b55d
Rob <robcalcote@gmail.com>
2018-10-22 22:34:57 -0600

    Added a GuestReponses model to store the data of the RSVP form

---
+++ ViewsRFun/Models/GuestReponses.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ViewsRFun.Models
+{
+    public class GuestReponses
+    {
+        // These are variables within the model - Name, Email, Phone, WillAttend
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool? WillAttend { get; set; }
+    }
+}
