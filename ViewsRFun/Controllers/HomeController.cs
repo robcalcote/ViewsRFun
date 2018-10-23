0cae4ab50892bffdb9c1dc53a13744362a7f3566
Rob <robcalcote@gmail.com>
2018-10-22 22:18:41 -0600

    Added Index View and Home controller - experimenting with views.

---
+++ ViewsRFun/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViewsRFun.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+
+            return View();
+        }
+    }
+}
